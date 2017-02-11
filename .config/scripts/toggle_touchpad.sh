0979288c451ae974c1e900561140f20fc8fc7a96
tlegutko <tom.legutko@gmail.com>
2017-02-11 03:25:32 +0100

    i3 normal mode and correct cursor hiding

--- .config/scripts/toggle_touchpad.sh
+++ .config/scripts/toggle_touchpad.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
 CURR=$(xinput list-props "SynPS/2 Synaptics TouchPad" | grep "Device Enabled" | cut -f 3)
-if [ $CURR -eq 0 ]; then 
-  NEXT=1
-  WS_POS=$(i3-msg -t get_workspaces | jq '.[] | select(.focused==true).rect | .x+.width/2,.y+.height/2')
-  xdotool mousemove ${WS_POS[0]} ${WS_POS[1]}
-else 
-  NEXT=0
-  xdotool mousemove 3840 2160 # hide
-fi
+if [ $CURR -eq 0 ]; then NEXT=1; else NEXT=0; fi
 xinput set-prop "SynPS/2 Synaptics TouchPad" "Device Enabled" $NEXT
--- .vimrc
+++ .vimrc
@@ -8,4 +8,5 @@ autocmd FileType * setlocal formatoptions-=c formatoptions-=r formatoptions-=o
 set expandtab
 set shiftwidth=2
 set softtabstop=2
+set autoindent
 cmap w!! w !sudo tee > /dev/null %
