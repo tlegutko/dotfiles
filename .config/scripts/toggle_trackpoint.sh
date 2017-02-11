2b17ead6bc12ca57fc549015ac6bf2896f6574d3
tlegutko <tom.legutko@gmail.com>
2017-02-11 03:37:43 +0100

    touchpad/trackpoint are moved to middle of the screen when enabled

--- .config/scripts/toggle_trackpoint.sh
+++ .config/scripts/toggle_trackpoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 CURR=$(xinput list-props "TPPS/2 IBM TrackPoint" | grep "Device Enabled" | cut -f 3)
-if [ $CURR -eq 0 ]; then NEXT=1; else NEXT=0; fi
+if [ $CURR -eq 0 ]; then 
+  NEXT=1
+  WS_POS=$(i3-msg -t get_workspaces | jq '.[] | select(.focused==true).rect | .x+.width/2,.y+.height/2')
+  xdotool mousemove ${WS_POS[0]} ${WS_POS[1]}
+else 
+  NEXT=0
+fi
 xinput set-prop "TPPS/2 IBM TrackPoint" "Device Enabled" $NEXT
