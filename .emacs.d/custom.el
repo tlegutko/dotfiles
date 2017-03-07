da59f6b59f2df63d56012b2995c8245124e1d96a
tlegutko <tom.legutko@gmail.com>
2017-03-07 01:18:41 +0100

    Functional ensime config.

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,7 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (linum-relative zenburn-theme monokai-theme evil))))
+ '(line-number-mode nil)
+ '(org-agenda-files
+   (quote
+    ("~/org/scala.org" "~/org/masters-thesis.org" "~/org/dance-notes.org")))
+ '(package-selected-packages
+   (quote
+    (smartparens ensime use-package linum-relative pdf-tools evil zenburn-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,8 +1,27 @@
-;;; melpa repo
+;; the package manager
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(setq
+ package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+		    ("org" . "http://orgmode.org/elpa/")
+		    ("melpa" . "http://melpa.org/packages/")
+		    ("melpa-stable" . "http://stable.melpa.org/packages/"))
+ package-archive-priorities '(("melpa-stable" . 1)))
+
+;; my list of packages to auto install
+(setq package-list '(zenburn-theme evil pdf-tools org linum-relative use-package))
 (package-initialize)
+
+; fetch the list of packages available 
+(unless package-archive-contents
+  (package-refresh-contents))
+
+; install the missing packages
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
+
+;; use-package always download listed packages
+(setq use-package-always-ensure t)
 
 ;;; look & feel
 (load-theme 'zenburn t)
@@ -11,11 +30,21 @@
 (scroll-bar-mode -1)
 ;; font is 1/10 of height
 (set-face-attribute 'default nil :height 80)
+;; auto-save loaded buffers
+(desktop-save-mode 1)
 
 ;;; evil
 (require 'evil)
-(define-key evil-normal-state-map (kbd "C-u") 'evil-scroll-up)
 (evil-mode 1)
+;; remove binding for ensime's go to definition
+(define-key evil-normal-state-map (kbd "M-.") nil)
+;; C-k C-j for scroll up/down
+(define-key evil-normal-state-map (kbd "C-k") (lambda ()
+						(interactive)
+						(evil-scroll-up nil)))
+(define-key evil-normal-state-map (kbd "C-j") (lambda ()
+						(interactive)
+						(evil-scroll-down nil)))
 
 ;;; eval shortcut
 (global-set-key (kbd "C-x E") 'eval-buffer)
@@ -37,19 +66,92 @@
 (defun transpose-windows ()
   (interactive)
   (let ((this-buffer (window-buffer (selected-window)))
-        (other-buffer (prog2
-                          (other-window +1)
-                          (window-buffer (selected-window))
-                        (other-window -1))))
+	(other-buffer (prog2
+			  (other-window +1)
+			  (window-buffer (selected-window))
+			(other-window -1))))
     (switch-to-buffer other-buffer)
     (switch-to-buffer-other-window this-buffer)
     (other-window -1)))
 
 ;;; line numbers
 (require 'linum-relative)
+(global-linum-mode t)
 (linum-relative-on)
 (setq linum-relative-current-symbol "")
 
 ;;; automatic custom variables
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
+
+;;; org mode
+(require 'org)
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-cb" 'org-iswitchb)
+; line wrapping
+(setq org-startup-truncated 'nil)
+(global-visual-line-mode t)
+(define-key org-mode-map "\M-q" 'toggle-truncate-lines)
+
+;; store all backup and autosave files in the tmp dir
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
+;; smart partentheses
+(use-package smartparens
+  :diminish smartparens-mode
+  :commands
+  smartparens-strict-mode
+  smartparens-mode
+  sp-restrict-to-pairs-interactive
+  sp-local-pair
+  :init
+  (setq sp-interactive-dwim t)
+  :config
+  (require 'smartparens-config)
+  (sp-use-smartparens-bindings)
+
+  (sp-pair "(" ")" :wrap "C-(") ;; how do people live without this?
+  (sp-pair "[" "]" :wrap "s-[") ;; C-[ sends ESC
+  (sp-pair "{" "}" :wrap "C-{")
+
+  ;; WORKAROUND https://github.com/Fuco1/smartparens/issues/543
+  (bind-key "C-<left>" nil smartparens-mode-map)
+  (bind-key "C-<right>" nil smartparens-mode-map)
+
+  (bind-key "s-<delete>" 'sp-kill-sexp smartparens-mode-map)
+  (bind-key "s-<backspace>" 'sp-backward-kill-sexp smartparens-mode-map))
+
+;; scala
+(use-package scala-mode
+  :defer t
+  :pin melpa
+  :init
+  (setq
+   scala-indent:use-javadoc-style t
+   scala-indent:align-parameters t)
+  :config
+
+  ;; prefer smartparens for parens handling
+  (remove-hook 'post-self-insert-hook
+               'scala-indent:indent-on-parentheses)
+
+  (sp-local-pair 'scala-mode "(" nil :post-handlers '(("||\n[i]" "RET")))
+  (sp-local-pair 'scala-mode "{" nil
+               :post-handlers '(("||\n[i]" "RET")
+                                ("| " "SPC"))))
+;; ensime
+(use-package ensime
+  :pin melpa-stable)
+
+;; scala-mode hooks
+(add-hook 'scala-mode-hook
+          (lambda ()
+            (show-paren-mode t)
+            (smartparens-mode t)
+	    (ensime-mode t)
+	    (scala-mode:goto-start-of-code)))
