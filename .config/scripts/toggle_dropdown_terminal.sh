8b78c59c5453358295ea0a708332c3e16ef85026
tlegutko <tom.legutko@gmail.com>
2017-04-20 18:04:38 +0200

    One of dropdowns is now x-emacs.

--- .config/scripts/toggle_dropdown_terminal.sh
+++ .config/scripts/toggle_dropdown_terminal.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 terminal_num=$1
-is_dropdown=$(xprop -id `xdotool getactivewindow` | grep -c "WM_WINDOW_ROLE(STRING) = \"${terminal_num}-dropdown-termite\"")
+is_dropdown=$(xprop -id `xdotool getactivewindow` | grep -c "WM_NAME(STRING) = \"${terminal_num}-dropdown\"")
 if ((is_dropdown == 1)); then
-  i3-msg "[window_role="1-dropdown-termite"] move scratchpad"
-  i3-msg "[window_role="2-dropdown-termite"] move scratchpad"
+  i3-msg "[title="1-dropdown"] move scratchpad"
+  i3-msg "[title="2-dropdown"] move scratchpad"
 else
   ws=($(i3-msg -t get_workspaces | jq '.[] | select(.focused==true) | .rect | .x,.y,.width,.height/2 | floor'))
-  i3-msg "[window_role="${terminal_num}-dropdown-termite"] scratchpad show; [window_role="${terminal_num}-dropdown-termite"] move position ${ws[0]} ${ws[1]}; resize set ${ws[2]} ${ws[3]}"
+  i3-msg "[title="${terminal_num}-dropdown"] scratchpad show; [title="${terminal_num}-dropdown"] move position ${ws[0]} ${ws[1]}; resize set ${ws[2]} ${ws[3]}"
 fi
