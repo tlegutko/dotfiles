77d5d0f83c790e7ad7dcc7852fe047fd273540f8
tlegutko <tom.legutko@gmail.com>
2017-05-18 17:44:10 +0200

    Improved i3wm keybindings ergonomics. Mod5 instead of Mod4, command mode.

--- .config/scripts/toggle_dropdown_terminal.sh
+++ .config/scripts/toggle_dropdown_terminal.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 terminal_num=$1
-is_dropdown=$(xprop -id `xdotool getactivewindow` | grep -c "WM_NAME(STRING) = \"${terminal_num}-dropdown\"")
-if ((is_dropdown == 1)); then
-  i3-msg "[title="1-dropdown"] move scratchpad"
-  i3-msg "[title="2-dropdown"] move scratchpad"
+is_last_activated_dropdown=$(xprop -id `xdotool getactivewindow` | grep -c "WM_NAME(STRING) = \"${terminal_num}-dropdown\"")
+if ((is_last_activated_dropdown == 1)); then
+    for i in `seq 0 4`; do
+	i3-msg "[title="${i}-dropdown"] move scratchpad"
+    done    
 else
   ws=($(i3-msg -t get_workspaces | jq '.[] | select(.focused==true) | .rect | .x,.y,.width,.height/2 | floor'))
   i3-msg "[title="${terminal_num}-dropdown"] scratchpad show; [title="${terminal_num}-dropdown"] move position ${ws[0]} ${ws[1]}; resize set ${ws[2]} ${ws[3]}"
