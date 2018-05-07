235908e8a5d07e6591c002043fd472407704c2e2
tlegutko <tom.legutko@gmail.com>
2018-05-07 19:38:40 +0200

    Added EXWM initial configuration

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -108,6 +108,7 @@
 (global-set-key (kbd "C-x K") 'kill-this-buffer)
 (global-unset-key (kbd "C-z"))
 (setq sentence-end-double-space nil)
+(global-set-key (kbd "C-c s") 'isearch-forward)
 
 (defun switch-to-previous-buffer ()
   "Switch to previously open buffer.
@@ -265,6 +266,7 @@ Repeated invocations toggle between the two most recently open buffers."
    ("C-M-n" . org-metadown)
    ("M-p" . org-metaright)
    ("M-n" . org-metaleft)
+   ([C-M-return] . org-insert-todo-heading)
    ([C-tab] . nil)
    ("C-a" . nil)
    ([return] . org-return-indent)
@@ -537,8 +539,7 @@ Repeated invocations toggle between the two most recently open buffers."
 (use-package undo-tree
   :diminish undo-tree-mode
   :bind
-  (:map undo-tree-map
-	("C-M-/" . undo-tree-redo))
+  (("C-M-/" . undo-tree-redo))
   :config (global-undo-tree-mode))
 
 (global-set-key (kbd "C-;") 'comment-line)
@@ -629,8 +630,11 @@ Repeated invocations toggle between the two most recently open buffers."
   :init
   (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
   (setq aw-scope 'global)
+  (setq aw-minibuffer-flag t)
   :bind
-  (([C-tab] . ace-window)))
+  (([C-tab] . ace-window))
+  :config
+  (ace-window-display-mode))
 
 (use-package winner
   :config
@@ -735,12 +739,109 @@ Repeated invocations toggle between the two most recently open buffers."
   :config
   (dired-async-mode 1))
 
-(use-package atomic-chrome
-  :init
-  (setq atomic-chrome-buffer-open-style 'split)
-  :config
-  (atomic-chrome-start-server))
-
 (delete-selection-mode 1)
 
 (use-package gnuplot)
+
+(use-package exwm
+  :config
+  (require 'exwm-config)
+  (require 'exwm-randr)
+  ;; Set the initial number of workspaces (they can also be created later).
+  (setq exwm-workspace-number 5)
+  (setq exwm-randr-workspace-output-plist '(1 "DP-4" 2 "DP-1" 3 "DP-4" 4 "DP-1"))
+  (add-hook 'exwm-randr-screen-change-hook
+          (lambda ()
+            (start-process-shell-command
+             "xrandr" nil "xrandr --output DP-4 --auto --primary --output DP-1 --auto --right-of DP-1")))
+  (exwm-randr-enable)
+  
+  ;; All buffers created in EXWM mode are named "*EXWM*". You may want to
+  ;; change it in `exwm-update-class-hook' and `exwm-update-title-hook', which
+  ;; are run when a new X window class name or title is available.  Here's
+  ;; some advice on this topic:
+  ;; + Always use `exwm-workspace-rename-buffer` to avoid naming conflict.
+  ;; + For applications with multiple windows (e.g. GIMP), the class names of
+  ;    all windows are probably the same.  Using window titles for them makes
+  ;;   more sense.
+  ;; In the following example, we use class names for all windows expect for
+  ;; Java applications and GIMP.
+  (add-hook 'exwm-update-class-hook
+            (lambda ()
+              (unless (or (string-prefix-p "sun-awt-X11-" exwm-instance-name)
+                          (string= "gimp" exwm-instance-name))
+                (exwm-workspace-rename-buffer exwm-class-name))))
+  (add-hook 'exwm-update-title-hook
+            (lambda ()
+              (when (or (not exwm-instance-name)
+                        (string-prefix-p "sun-awt-X11-" exwm-instance-name)
+                        (string= "gimp" exwm-instance-name))
+                (exwm-workspace-rename-buffer exwm-title))))
+  
+  ;; Global keybindings can be defined with `exwm-input-global-keys'.
+  ;; Here are a few examples:
+  (setq exwm-input-global-keys
+        `(
+          ;; Bind "s-r" to exit char-mode and fullscreen mode.
+          ([?\s-r] . exwm-reset)
+          ;; Bind "s-w" to switch workspace interactively.
+          ([?\s-w] . exwm-workspace-switch)
+          ;; Bind "s-0" to "s-9" to switch to a workspace by its index.
+          ,@(mapcar (lambda (i)
+                      `(,(kbd (format "s-%d" i)) .
+                        (lambda ()
+                          (interactive)
+                          (exwm-workspace-switch-create ,i))))
+                    (number-sequence 0 9))
+          ;; Bind "s-&" to launch applications ('M-&' also works if the output
+          ;; buffer does not bother you).
+          ([?\s-&] . (lambda (command)
+    		     (interactive (list (read-shell-command "$ ")))
+  		     (start-process-shell-command command nil command)))
+          ;; Bind "s-<f2>" to "slock", a simple X display locker.
+          ([s-f2] . (lambda ()
+  		    (interactive)
+  		    (start-process "" nil "/usr/bin/slock")))))
+  
+  ;; To add a key binding only available in line-mode, simply define it in
+  ;; `exwm-mode-map'.  The following example shortens 'C-c q' to 'C-q'.
+  (define-key exwm-mode-map [?\C-q] #'exwm-input-send-next-key)
+  
+  ;; The following example demonstrates how to use simulation keys to mimic
+  ;; the behavior of Emacs.  The value of `exwm-input-simulation-keys` is a
+  ;; list of cons cells (SRC . DEST), where SRC is the key sequence you press
+  ;; and DEST is what EXWM actually sends to application.  Note that both SRC
+  ;; and DEST should be key sequences (vector or string).
+  (setq exwm-input-simulation-keys
+        '(
+          ;; movement
+          ([?\C-b] . [left])
+          ([?\M-b] . [C-left])
+          ([?\C-f] . [right])
+          ([?\M-f] . [C-right])
+          ([?\C-p] . [up])
+          ([?\C-n] . [down])
+          ([?\C-a] . [home])
+          ([?\C-e] . [end])
+          ([?\M-v] . [prior])
+          ([?\C-v] . [next])
+          ([?\C-d] . [delete])
+          ([?\C-k] . [S-end delete])
+          ;; cut/paste.
+          ([?\C-w] . [?\C-x])
+          ([?\M-w] . [?\C-c])
+          ([?\C-y] . [?\C-v])
+          ;; search
+          ([?\C-s] . [?\C-f])))
+  
+  ;; You can hide the minibuffer and echo area when they're not used, by
+  ;; uncommenting the following line.
+  ;; (setq exwm-workspace-minibuffer-position 'bottom)
+
+  ;; (require 'exwm-systemtray)
+  ;; (exwm-systemtray-enable)
+  
+  ;; Do not forget to enable EXWM. It will start by itself when things are
+  ;; ready.  You can put it _anywhere_ in your configuration.
+  (exwm-enable))
+  
--- .zprofile
+++ .zprofile
@@ -1,5 +1,7 @@
 [ -f ~/.zshrc ] && . ~/.zshrc
 export PATH="$PATH:/home/tlegutko/.npm-global/bin"
-if [ -z "$DISPLAY" ] && [ -n "$XDG_VTNR" ] && [ "$XDG_VTNR" -eq 1 ]; then
+if [ -z "$DISPLAY" -a "$(tty)" = '/dev/tty2' ]; then
+    exec xinit /home/tlegutko/.xinitrc-exwm -- vt02
+elif [ -z "$DISPLAY" ] && [ -n "$XDG_VTNR" ] && [ "$XDG_VTNR" -eq 1 ]; then
   exec startx
 fi
